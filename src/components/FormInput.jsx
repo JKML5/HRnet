5a77ff3c0db0e658029955c4a0a0d58546b5a2fe
JK <jk.floods@gmail.com>
2023-02-21 14:00:58 +0100

    fix styled components

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,29 +1,28 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import '../css/FormInput.css';
+
+const StyledInput = styled.input`
+  font-size: 16px;
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  border: 1px solid #000000;
+  outline: none;
+  border-radius: 5px;
+
+  &.half-width {
+    width: 50%;
+  }
+
+  &:focus {
+    border: none;
+    outline: 2px solid #4e6701;
+  }
+`;
 
 function FormInput({ id, name, type, onChange, value }) {
-  // const StyledInput = styled.input`
-  //   font-size: 16px;
-  //   width: 100%;
-  //   height: 40px;
-  //   padding: 0 10px;
-  //   border: 1px solid #000000;
-  //   outline: none;
-  //   border-radius: 5px;
-
-  //   &.half-width {
-  //     width: 50%;
-  //   }
-
-  //   &:focus {
-  //     border: none;
-  //     outline: 2px solid #4e6701;
-  //   }
-  // `;
-
   return (
-    <input
+    <StyledInput
       type={type}
       id={id}
       className="inputText"
