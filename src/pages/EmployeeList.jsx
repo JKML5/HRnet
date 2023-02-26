5c51e49f56bb65dd2ecd5deb125b4f40c53ad06c
JK <jk.floods@gmail.com>
2023-02-26 23:43:14 +0100

    react table with filter

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { useTable, useSortBy } from 'react-table';
+import { useTable, useGlobalFilter, useSortBy } from 'react-table';
 import Title from '../components/Title';
 
 const StyledTable = styled.table`
@@ -33,49 +33,73 @@ function EmployeeList({ employees }) {
       {
         Header: 'Last Name',
         accessor: 'lastName', // accessor is the "key" in the data
+        filterAll: true,
       },
       {
         Header: 'First Name',
         accessor: 'firstName',
+        filterAll: true,
       },
       {
         Header: 'Start date',
         accessor: (data) => data.dateStart.toLocaleString(),
+        filterAll: true,
       },
       {
         Header: 'Department',
         accessor: 'department',
+        filterAll: true,
       },
       {
         Header: 'Date of birth',
         accessor: (data) => data.dateBirth.toLocaleString(),
+        filterAll: true,
       },
       {
         Header: 'Street',
         accessor: 'street',
+        filterAll: true,
       },
       {
         Header: 'City',
         accessor: 'city',
+        filterAll: true,
       },
       {
         Header: 'State',
         accessor: 'state',
+        filterAll: true,
       },
       {
         Header: 'Zip code',
         accessor: 'zipCode',
+        filterAll: true,
       },
     ],
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data: employees }, useSortBy);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({ columns, data: employees }, useGlobalFilter, useSortBy);
+
+  const { globalFilter } = state;
 
   return (
     <main className="main">
       <Title value="Current Employees" />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={globalFilter || ''}
+        onChange={(e) => setGlobalFilter(e.target.value)}
+      />
 
       <div id="employee-div" className="container">
         <StyledTable
